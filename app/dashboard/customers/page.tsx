56836e36be195c3373b2004ca4b7ebe37b993e93
JBR-0100 <jbr.the.programmer@gmail.com>
2025-05-02 18:39:54 +0530

    tried_again

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -6,16 +6,15 @@ export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams?: {
+export default async function Page(props: {
+  searchParams?: Promise<{
     query?: string;
-  };
+    page?: string;
+  }>;
 }) {
+  const searchParams = await props.searchParams;
   const query = searchParams?.query || '';
   const customers = await fetchFilteredCustomers(query);
-
   return (
     <main className="w-full">
       <CustomersTable customers={customers} />
