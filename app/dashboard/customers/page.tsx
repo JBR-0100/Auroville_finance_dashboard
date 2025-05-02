84e1fadd32d0b0f71a450856c61e75d72de50bc1
JBR-0100 <jbr.the.programmer@gmail.com>
2025-05-02 18:34:00 +0530

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,24 @@
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <main className="w-full">
+      <CustomersTable customers={customers} />
+    </main>
+  );
+}
